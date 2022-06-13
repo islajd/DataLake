aae76db60294b8db7338744ac5577960c4b2640a
islajd <islajdm@gmail.com>
2022-06-13 21:49:05 +0200

    Initial commit

---
+++ test/data-lake.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const DataLake = require('../lib/data-lake-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/data-lake-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new DataLake.DataLakeStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
